135c57edeeb1dabc48705f6addf191a8b49515d7
임진영 <delheure@gmail.com>
2021-02-04 22:12:24 +0900

    Init : to do list

---
+++ script.js
@@ -0,0 +1,40 @@
+const input = document.querySelector(".todo_txt");
+const btn = document.querySelector(".btn_submit");
+const todoList = document.querySelector(".todo_list");
+
+const addTodo = e => {
+  e.preventDefault();
+
+  if (input.value.length < 1) {
+    return;
+  }
+
+  const list = document.createElement("li");
+
+  const btnChk = document.createElement("button");
+  btnChk.setAttribute("class", "btn_check");
+  btnChk.addEventListener("click", e => {
+    e.target.classList.toggle("on");
+  });
+
+  const btnDel = document.createElement("button");
+  btnDel.setAttribute("class", "btn_del");
+  btnDel.addEventListener("click", () => {
+    todoList.removeChild(list);
+  });
+
+  todoList.appendChild(list);
+  list.appendChild(btnChk);
+  list.insertAdjacentHTML("beforeend", `${input.value}`);
+  list.appendChild(btnDel);
+
+  input.value = "";
+};
+
+btn.addEventListener("click", addTodo);
+input.addEventListener("keypress", e => {
+  if (e.key === "Enter") {
+    addTodo();
+  }
+  return;
+});
